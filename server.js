a5394676c63f4d99652320815b0f936e3883e48d
Jon Moses <jon@burningbush.us>
2011-03-31 19:58:24 -0400

    more

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var fs = require('fs'),
+    _ = require('underscore');
 
 var server = require('http').createServer(function(req, response){
   fs.readFile('index.html', function(err, data){
@@ -10,8 +11,25 @@ var server = require('http').createServer(function(req, response){
 server.listen(8080);
 
 var everyone = require('now').initialize(server);
+var messages = [];
+var message_limit = 100;
 
 everyone.now.distributeMessage = function(message) {
+  messages.push([this.now.name, message]);
+  if( messages.length > message_limit ) {
+    messages = messages[messages.length - message_limit-1, messages.length-1];
+  }
   everyone.now.receiveMessage(this.now.name, message);
 };
+
+everyone.now.joined = function() {
+  _.each(messages, _.bind(function(message) {
+    this.now.receiveMessage(message[0], message[1]);
+  }, this));
+  this.now.receiveMessage("System", "Welcome, " + this.now.name);
+};
+
+setInterval(function() {
+  everyone.now.timestamp();
+}, 60000);
 
