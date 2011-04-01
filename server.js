d5b4ae48916f760877f16bab9b25bf1976f92fb8
Jon Moses <jon@burningbush.us>
2011-04-01 12:23:09 -0400

    rudimentary login + /commands

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var fs = require('fs'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    static = require('node-static'),
+    file = new(static.Server)('./public');
 
-var server = require('http').createServer(function(req, response){
-  fs.readFile('index.html', function(err, data){
-    response.writeHead(200, {'Content-Type':'text/html'}); 
-    response.write(data);  
-    response.end();
+var server = require('http').createServer(function(req, res){
+  req.addListener('end', function() {
+    file.serve(req, res);
   });
 });
 server.listen(8080);
@@ -15,6 +15,8 @@ var messages = [];
 var message_limit = 100;
 
 everyone.now.distributeMessage = function(message) {
+  if( _.isUndefined(this.now.name) ) { return; }
+
   messages.push([this.now.name, message]);
   if( messages.length > message_limit ) {
     messages = messages[messages.length - message_limit-1, messages.length-1];
@@ -22,11 +24,22 @@ everyone.now.distributeMessage = function(message) {
   everyone.now.receiveMessage(this.now.name, message);
 };
 
-everyone.now.joined = function() {
+everyone.now.command = function(command) {
+  switch(command) {
+    case 'time':
+      this.now.receiveMessage('System', 'Time');
+      break;
+    default:
+      this.now.receiveMessage('System', 'Huh??');
+      break;
+  }
+}
+
+everyone.now.joined = function(name) {
   _.each(messages, _.bind(function(message) {
     this.now.receiveMessage(message[0], message[1]);
   }, this));
-  this.now.receiveMessage("System", "Welcome, " + this.now.name);
+  this.now.receiveMessage("System", "Welcome, " + (name || this.now.name));
 };
 
 setInterval(function() {
